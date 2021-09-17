46135a43e47060cb11973d539665a131dc7904db
tttyuntian <tttyuntian@hotmail.com>
2021-09-17 12:02:28 -0400

    feat: add argument flag in piqa.precompute_embedding script

--- scripts/piqa/precompute_sentence_embedding.sh
+++ scripts/piqa/precompute_sentence_embedding.sh
@@ -1,7 +1,13 @@
-EMBEDDER=$1
+# Get arguments
+while getopts e: flag
+do
+    case "${flag}" in
+        e) embedder=${OPTARG};;
+    esac
+done
 
 # Run experiment
 python3 -m vlm_lexical_grounding.piqa.precompute_sentence_embedding \
     --verbose \
     --num_rows -1 \
-    --embedder $EMBEDDER \
+    --embedder $embedder \
